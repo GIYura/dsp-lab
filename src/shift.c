3a227aa58e1622138d400ad3c5d0ed1286b3e4ec
jura <jglotovyn@gmail.com>
2026-09-24 14:16:41 +0300

    ADD: spectrum shift (13.1)

--- src/shift.c
+++ src/shift.c
@@ -19,3 +19,98 @@ void SignalShift(const double* const in, const complex_t* const shift, complex_t
         out[i] = ComplexMul(value, shift[i]);
     }
 }
+
+/*
+ * NOTE: DSP 13.1.1
+ * */
+void ShiftOnHalfOfSampleFreq(const double* const src, double* const dst, uint32_t size)
+{
+    assert(src != NULL);
+    assert(dst != NULL);
+    assert(size > 0);
+
+    for (uint32_t i = 0; i < size; i++)
+    {
+        if (i % 2 == 0)
+        {
+            dst[i] = src[i];
+        }
+        else
+        {
+            dst[i] = -src[i];
+        }
+    }
+}
+
+/*
+ * NOTE: DSP 13.1.2
+ * */
+void ShiftMinusQuaterFs(const double* const src, complex_t* const dst, uint32_t size)
+{
+    assert(src != NULL);
+    assert(dst != NULL);
+    assert(size > 0);
+
+    for (uint32_t i = 0; i < size; i++)
+    {
+        switch (i % 4U)
+        {
+        case 0: /* × (+1) */
+            dst[i].real = src[i];
+            dst[i].imag = 0.0;
+            break;
+
+        case 1: /* × (-j) */
+            dst[i].real = 0.0;
+            dst[i].imag = -src[i];
+            break;
+
+        case 2: /* × (-1) */
+            dst[i].real = -src[i];
+            dst[i].imag = 0.0;
+            break;
+
+        case 3: /* × (+j) */
+            dst[i].real = 0.0;
+            dst[i].imag = src[i];
+            break;
+        }
+    }
+}
+
+/*
+ * NOTE: DSP 13.1.2
+ * */
+void ShiftPlusQuaterFs(const double* const src, complex_t* const dst, uint32_t size)
+{
+    assert(src != NULL);
+    assert(dst != NULL);
+    assert(size > 0);
+
+    for (uint32_t i = 0; i < size; i++)
+    {
+        switch (i % 4U)
+        {
+        case 0: /* × (+1) */
+            dst[i].real = src[i];
+            dst[i].imag = 0.0;
+            break;
+
+        case 1: /* × (+j) */
+            dst[i].real = 0.0;
+            dst[i].imag = src[i];
+            break;
+
+        case 2: /* × (-1) */
+            dst[i].real = -src[i];
+            dst[i].imag = 0.0;
+            break;
+
+        case 3: /* × (-j) */
+            dst[i].real = 0.0;
+            dst[i].imag = -src[i];
+            break;
+        }
+    }
+}
+
--- src/shift.h
+++ src/shift.h
@@ -6,5 +6,8 @@
 #include "complex.h"
 
 void SignalShift(const double* const in, const complex_t* const shift, complex_t* const out, uint32_t size);
+void ShiftOnHalfOfSampleFreq(const double* const src, double* const dst, uint32_t size);
+void ShiftMinusQuaterFs(const double* const src, complex_t* const dst, uint32_t size);
+void ShiftPlusQuaterFs(const double* const src, complex_t* const dst, uint32_t size);
 
 #endif /* SHIFT_H */
